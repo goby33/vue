d55ad386e092b7468faafe756daf9c6a5ac3f0ee
Martin <martin.gobert@adeo.com>
2021-02-02 14:28:37 +0100

    add: exo1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <exo1 />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import exo1 from './components/exo1.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    exo1
   }
 }
 </script>
--- src/components/exo1.vue
+++ src/components/exo1.vue
@@ -1,10 +1,35 @@
 <template>
-$END$
+  <div>
+    <label>Text: <input v-model="text"></label>
+    <p v-if="showText">{{ text }}</p>
+    <ul v-if="showArray">
+      <li v-for="item in array" :key="item.id">{{ item.name }}</li>
+    </ul>
+    <button @click="showText = !showText">Afficher texte</button>
+    <button @click="showArray = !showArray">Afficher tableau</button>
+  </div>
 </template>
 
 <script>
 export default {
-name: "exo1"
+  name: "exo1",
+  data() {
+    return {
+      text: '',
+      showText: false,
+      array: [
+        {
+          id: 0,
+          name: 'my name'
+        },
+        {
+          id: 1,
+          name: 'etrvdf'
+        },
+      ],
+      showArray: false
+    }
+  }
 }
 </script>
 
